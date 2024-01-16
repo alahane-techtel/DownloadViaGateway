61b2928eff5df094c03e6060041e58b0d3b6ca7e
DESKTOP-O1VMJ4K\Ashish Lahane <alahane@techtel.com.au>
2024-01-16 15:18:18 +1100

    first

---
+++ DownloadViaGateway/Controllers/DownloadController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace DownloadViaGateway.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DownloadController : ControllerBase
+    {
+        private readonly ILogger<DownloadController> _logger;
+
+        public DownloadController(ILogger<DownloadController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet("BigFile")]
+        //public async Task<FileStreamResult> BigFile()
+        //{
+        //    var fileName = "big.mp4";
+        //    var stream = System.IO.File.OpenRead(Path.Combine("Files", fileName));
+
+        //    Response.Headers.Add("Content-Disposition", $"attachment; filename={fileName}");
+        //    return new FileStreamResult(stream, "application/octet-stream");
+        //}
+
+        [HttpGet("SmallFile")]
+        public async Task<FileStreamResult> SmallFile()
+        {
+            var fileName = "small.mp4";
+            var stream = System.IO.File.OpenRead(Path.Combine("Files", fileName));
+
+            Response.Headers.Add("Content-Disposition", $"attachment; filename={fileName}");
+            return new FileStreamResult(stream, "application/octet-stream");
+        }
+    }
+}
